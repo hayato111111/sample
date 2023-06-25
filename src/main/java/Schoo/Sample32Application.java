21bcdd35f1f9de7181cfaf94793141e4140fca7f
hayato0615 <s1932102CT@s.chibakoudai.jp>
2023-06-25 20:56:10 +0900

    test

---
+++ src/main/java/Schoo/Sample32Application.java
@@ -0,0 +1,13 @@
+package Schoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sample32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sample32Application.class, args);
+	}
+
+}
---
+++ src/test/java/Schoo/Sample32ApplicationTests.java
@@ -0,0 +1,13 @@
+package Schoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sample32ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
